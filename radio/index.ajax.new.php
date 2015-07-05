b6ab762ab68e3d5e6fb44ab52a8abd8a1ce291cd
gaoerjun <jiayougej@163.com>
2015-07-05 14:37:44 +0800

    完成了radio模块的定制开发

--- radio/index.ajax.new.php
+++ radio/index.ajax.new.php
@@ -3,23 +3,24 @@
     <title>天主教小助手网络电台</title>
     <meta http-equiv=Content-Type content="text/html;charset=utf-8">
     <meta name="viewport" content="user-scalable=no, width=device-width, minimal-ui"/>
+    <link rel="stylesheet" type="text/css" href="/js/jDateSelect/css/jqueryMobile.css"/>
+    <link rel="stylesheet" type="text/css" href="/js/jDateSelect/css/mobiscroll.css"/>
     <link rel="stylesheet" type="text/css" href="/js/jPlayer/skin/cd/cd.new.css"/>
+
     <style type="text/css">
-        .title-bar select {
-            padding: 5px;
-            font-size: 20px;
-            height: 100%;
-            -webkit-appearance: none;
-            border: 0 none;
-            background: transparent;
+        .jp-interface{
+            margin-bottom: 70px;
         }
     </style>
     <script type="text/javascript" src="http://cathassist.org/js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="/js/jqueryMobile.js"></script>
     <script type="text/javascript" src="http://cathassist.org/js/jPlayer/js/jquery.jplayer.min.js"></script>
     <script type="text/javascript" src="http://cathassist.org/js/jPlayer/js/jplayer.playlist.min.js"></script>
-    <script src="http://cathassist.org/js/snap.min.js"></script>
+
     <script type="text/javascript" language="javascript" src="http://cathassist.org/include/googleanalysis.js"></script>
     <script type="text/javascript" language="javascript" src="http://cathassist.org/include/common.js"></script>
+    <script type="text/javascript" language="javascript" src="/js/jDateSelect/jqueryMobile.js"></script>
+    <script type="text/javascript" language="javascript" src="/js/jDateSelect/mobiscroll.js"></script>
     <script>
         var ppl = null;
         var curDate = new Date();
@@ -66,6 +67,9 @@
                 ppl.setPlaylist(list);
                 if (typeof(list[1]) == "object") {
                     $(".jp-playlist").show();
+                    $("#right_menu_icon").removeClass("gray");
+                }else{
+                    $("#right_menu_icon").addClass("gray");
                 }
                 var title = channelDescMap[channel];
                 $("#jptitle").html(title);
@@ -87,7 +91,6 @@
             })
 
         }
-
         $(document).ready(function () {
             ppl = new jPlayerPlaylist({
                 jPlayer: "#jquery_jplayer_1",
@@ -120,6 +123,27 @@
                 smoothPlayBar: true,
                 keyEnabled: true
             });
+            // 初始化插件内容
+            var opt_data = {
+                preset: 'date', //日期格式 date（日期）|datetime（日期加时间）
+                theme: 'jqm', //皮肤样式
+                display: 'modal', //显示方式
+                mode: 'clickpick', //日期选择模式
+                dateFormat: 'yy-mm-dd', // 日期格式
+                setText: '确定', //确认按钮名称
+                cancelText: '取消',//取消按钮名籍我
+                dateOrder: 'yymmdd', //面板中日期排列格式
+                dayText: '日', monthText: '月', yearText: '年', //面板中年月日文字
+                yearText: '年', monthText: '月',  dayText: '日',  //面板中年月日文字
+                endYear:2020, //结束年份
+                showNow:true,
+                nowText:'今天',
+                hourText:'小时',
+                minuteText:'分'
+            };
+            // 使用定义插件
+            $("#dateBtn").mobiscroll(opt_data);
+
             var argC = getQueryString("channel");
 
             var argD = getQueryString("date");
@@ -135,9 +159,21 @@
              *切换二级列表页面
              */
             $('.open-right').click(function () {
-                $(".jp-playlist").toggle();
+                if(!$("#right_menu_icon").hasClass("gray")){
+                    $(".jp-playlist").toggle();
+                }else{
+                    return false;
+                }
             });
-
+            $(".jp-interface").scroll(function(){
+                var top = $(".jp-interface").scrollTop();
+                if(top>=0){
+                    var winHeight = $(window).height();
+                    var fixtop =(44-top)>=0?44-top:0;
+                    $(".snap-drawers").css({"top":fixtop,height:winHeight-70-fixtop});
+                    $(".snap-drawer").css("height",winHeight-70-fixtop);
+                }
+            })
             //获取一级列表
             $.get("getradio.php", function (data) {
                 data = jQuery.parseJSON(data);
@@ -170,7 +206,8 @@
                 getRadio(curDate);
                 updateHref();
             });
-
+            $(".ui-page").removeClass("ui-page").removeAttr("data-role").removeClass("ui-body-c ui-page-active");
+            $("#prevBtn,#nextBtn").prev("span").css("padding","0");
 
         });
 
@@ -184,7 +221,7 @@
     <div class="jp-type-playlist">
         <div class="jp-gui jp-interface">
             <div class="title-bar">
-                <a href="#" class="open-right"></a>
+                <a href="#" class="open-right"><img src="/js/jPlayer/skin/cd/icon_menu.png" class="gray" id="right_menu_icon"></a>
                     <span class="wrap">
                         <b id="jptitle"></b>
                     </span>
@@ -206,6 +243,12 @@
                 </ul>
             </div>
 
+            <div class="date-control">
+                <button id="prevBtn">上一天</button>
+                <input id="dateBtn" type="text" value="2014/03/08" />
+                <button id="nextBtn">下一天</button>
+            </div>
+
             <div class="cp-showbox">
                 <div class="control cp-control">
                     <div class="control-con">
@@ -221,11 +264,6 @@
                         <div class="jp-play-bar"></div>
                     </div>
                 </div>
-            </div>
-            <div class="date-control">
-                <button id="prevBtn">上一天</button>
-                <input id="dateBtn" type="date" value="2014/03/08" autocomplete="off"/>
-                <button id="nextBtn">下一天</button>
             </div>
         </div>
         <div class="jp-playlist snap-drawers">
