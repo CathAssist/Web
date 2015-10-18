2c6078e1c4135af5720df56c374a3545dcd1f66b
Peter <me@liyake.com>
2015-10-18 12:11:30 +0800

    更新每小时定时抓取的页面

--- 3rd/checkforhour.php
+++ 3rd/checkforhour.php
@@ -12,6 +12,8 @@
 	
 //	echo('<h1>更新电台数据</h1>');
 //	echo(file_get_contents(ROOT_WEB_URL.'radio/updatedata.php'));
+	echo('<h1>更新Radio->Music</h1>');
+	echo(file_get_contents(ROOT_WEB_URL.'music/grab_radio/grab.php'));
 	
 	echo('<h1>发送微博</h1>');
 	echo(check2updateweibo());
